f2ed16272a08ec7f7c4c49be665f24e62d0aa8c9
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2025-05-13 12:53:15 +0200

    security

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+csv_file_path = os.getenv('FILE_LOCATION')
+mapbox_token = os.getenv('MAPBOX_TOKEN')
+
 
 app = Flask(__name__)
 
@@ -9,7 +17,7 @@ def show_map_from_ssid():
     limit = request.args.get('limit', type=int, default=100)
 
     # Load the CSV data
-    df = pd.read_csv('CSV FILE')
+    df = pd.read_csv(csv_file_path)
     df.columns = df.columns.str.strip()
 
     # Filter the dataframe for the given SSID
@@ -20,14 +28,17 @@ def show_map_from_ssid():
     ]
 
     # Select relevant columns
-    df_filtered = filtered_df[['SSID', 'CurrentLatitude', 'CurrentLongitude', 'Type']].dropna()
+    df_filtered = filtered_df[['SSID', 'CurrentLatitude', 'CurrentLongitude', 'Type', 'MAC']].dropna()
+
+    # Remove duplicate MAC addresses
+    df_filtered = df_filtered.drop_duplicates(subset='MAC')
 
     # Limit the result
     df_filtered = df_filtered.head(limit)
 
     networks = df_filtered.to_dict(orient='records')
 
-    return render_template('index.html', networks=networks, ssid=ssid, limit=limit)
+    return render_template('index.html', networks=networks, ssid=ssid, limit=limit, mapbox_token=mapbox_token)
 
 if __name__ == '__main__':
     app.run(debug=True)
